3dcc7de1877f11ab8b421d63eca082cb38d46859
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-03-24 18:07:20 +0100

    feat: testimonials + icons update

--- src/components/Sections/WhyUsSection/WhyUsSection.tsx
+++ src/components/Sections/WhyUsSection/WhyUsSection.tsx
@@ -7,23 +7,19 @@ import styles from './WhyUsSection.module.scss';
 
 const categories = [
   {
-    imgSrc:
-      'https://d1yei2z3i6k35z.cloudfront.net/5400183/6538c81806a96_Untitleddesign-68.png',
+    imgSrc: '/laptop.png',
     desc: 'Rendszeres élő beszélgetések, webinarok, ahol felteheted kérdéseidet, megoszthatod tapasztalataidat, végig kisérünk az úton, lépésről-lépésre.',
   },
   {
-    imgSrc:
-      'https://d1yei2z3i6k35z.cloudfront.net/5400183/653a08ac3fc4d_Untitleddesign-77.png',
-    desc: 'Csatlakozz egy támogató és összetartó közösséghez ahol hasonló gondolkodásmódú embereket ismerhetsz meg, akik együtt fejlődnek.',
-  },
-  {
-    imgSrc:
-      'https://d1yei2z3i6k35z.cloudfront.net/5400183/6538ccb2ec5c9_Untitleddesign-70.png',
+    imgSrc: '/atom.png',
     desc: 'Tudományos és modern megközelítés: érthetően és átlátható kutatásokkal alátámasztva.',
   },
   {
-    imgSrc:
-      'https://d1yei2z3i6k35z.cloudfront.net/5400183/6538ce9ccb4d7_Untitleddesign-71.png',
+    imgSrc: '/mind.png',
+    desc: 'Csatlakozz egy támogató és összetartó közösséghez ahol hasonló gondolkodásmódú embereket ismerhetsz meg, akik együtt fejlődnek.',
+  },
+  {
+    imgSrc: '/monk.png',
     desc: 'Gyakorlatok a tudásod megerősítésére, amelyek elvégzésével valóban napról-napra érezheted a változást.',
   },
 ];
