a9b0b0162657ed47a5311b9c288eaa72a4b4b053
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-03-31 18:50:00 +0200

    fix: suspense for params

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 import { ToastContainer, toast } from 'react-toastify';
 
 import styles from './layout.module.scss';
 
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function Toaster() {
   const searchParams = useSearchParams();
 
   const success = searchParams.get('success');
@@ -30,20 +26,32 @@ export default function HomeLayout({
   }, [success, cancel]);
 
   return (
+    <ToastContainer
+      position="top-center"
+      autoClose={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  );
+}
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
     <div className={styles.homeLayout}>
       {/* <Header /> */}
       <main>{children}</main>
-      <ToastContainer
-        position="top-center"
-        autoClose={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
+      <Suspense>
+        <Toaster />
+      </Suspense>
     </div>
   );
 }
