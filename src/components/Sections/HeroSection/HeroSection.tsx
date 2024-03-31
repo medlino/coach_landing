b38cf2454f99605a1bd3b407ee8ad032957838d0
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-03-31 19:13:02 +0200

    fix: loader workaround

--- src/components/Sections/HeroSection/HeroSection.tsx
+++ src/components/Sections/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { RotatingLines } from 'react-loader-spinner';
 
 import { stripeCheckout } from '@/clientAPI/checkout';
 
@@ -12,12 +13,17 @@ import { Button } from '../../Button';
 import styles from './HeroSection.module.scss';
 
 export const HeroSection = () => {
+  const [firstLoad, setFirstLoad] = useState(true);
   const [showVolumeIcon, setShowVolumeIcon] = useState(true);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   const isSmallDevice = useMediaQuery(
     `only screen and ${getBreakpoints().maxSm}`
   );
+
+  useEffect(() => {
+    setFirstLoad(false);
+  }, []);
 
   const volumeUp = () => {
     if (videoRef.current) {
@@ -36,31 +42,44 @@ export const HeroSection = () => {
       </h1>
 
       <div className={styles.videoWrapper}>
-        {showVolumeIcon && !isSmallDevice && (
-          <img
-            src="/icons/volume-mute-fill.svg"
-            alt="volume"
-            onClick={volumeUp}
+        {firstLoad && (
+          <RotatingLines
+            visible={true}
+            width="96"
+            strokeColor="white"
+            strokeWidth="5"
+            animationDuration="0.75"
           />
         )}
-        <video
-          ref={videoRef}
-          controls
-          autoPlay
-          muted
-          playsInline
-          width={isSmallDevice ? 254 : 316}
-          height={isSmallDevice ? 450 : 560}
-          style={{
-            border: '1px white solid',
-          }}
-        >
-          <source
-            src="https://medlino-salonic.s3.eu-central-1.amazonaws.com/VSLJ%C3%93.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
+        {!firstLoad && (
+          <>
+            {showVolumeIcon && (
+              <img
+                src="/icons/volume-mute-fill.svg"
+                alt="volume"
+                onClick={volumeUp}
+              />
+            )}
+            <video
+              ref={videoRef}
+              controls
+              autoPlay
+              muted
+              playsInline
+              width={isSmallDevice ? 254 : 316}
+              height={isSmallDevice ? 450 : 560}
+              style={{
+                border: '1px white solid',
+              }}
+            >
+              <source
+                src="https://medlino-salonic.s3.eu-central-1.amazonaws.com/VSLJ%C3%93.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </>
+        )}
       </div>
 
       <Button text="CSATLAKOZOM" onClick={stripeCheckout} />
