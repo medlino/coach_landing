c5005afea1db40ce60860deb1fb130e47bdabbf1
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-04-02 20:16:48 +0200

    chore: docs update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href="/docs/aszf.pdf" download>
+      <a href="/docs/aszf_v1.pdf" download>
         Felhasználási feltételek
       </a>
       <span>|</span>
-      <a href="/docs/adatvedelmi_nyilatkozat.pdf" download>
+      <a href="/docs/adatvedelmi_nyilatkozat_v1.pdf" download>
         Adatvédelem és szabályzat
       </a>
     </footer>
