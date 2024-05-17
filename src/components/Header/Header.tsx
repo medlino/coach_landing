297c0c7b4f2fe62a1e10d3ca80311c607598f3d7
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-05-17 18:24:58 +0200

    fix: hydration for menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { MobileHeader } from './MobileHeader';
@@ -6,6 +6,16 @@ import { DesktopHeader } from './DesktopHeader';
 
 export const Header = () => {
   const isMobile = useMediaQuery('(max-width: 768px)');
+  const [clientSide, setClientSide] = useState(false);
+
+  useEffect(() => {
+    setClientSide(true);
+  }, []);
+
+  // Use a consistent placeholder during the server render and the initial client render
+  if (!clientSide) {
+    return null;
+  }
 
   return <>{isMobile ? <MobileHeader /> : <DesktopHeader />}</>;
 };
