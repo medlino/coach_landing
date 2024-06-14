10d78369185bcbcd0c3e1c3a1a3568bb4e335a6a
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-06-14 19:19:59 +0200

    fix: prod product map

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -9,7 +9,6 @@ export const productRoleMap: Record<string, { id: string; name: string }[]> =
         prod_Q7j4DjrPMOQgyJ: [{ id: '1229468577279770755', name: 'TAG' }],
       }
     : {
-        prod_PiwzLgsemmCJ18: [{ id: '1229468577279770755', name: 'TAG' }],
-        prod_Q7j3Vx68kflDV3: [{ id: '1229468577279770755', name: 'TAG' }],
-        prod_Q7j4DjrPMOQgyJ: [{ id: '1229468577279770755', name: 'TAG' }],
+        prod_QIDM1cMlwTO01V: [{ id: '1229468577279770755', name: 'TAG' }],
+        prod_QIDMquP7wpaYQT: [{ id: '1229468577279770755', name: 'TAG' }],
       };
