6a07d0b12768be15838ee4d549fa82d062f085ac
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-06-15 12:29:50 +0200

    fix: typo

--- src/components/Sections/HelpSection/HelpSection.tsx
+++ src/components/Sections/HelpSection/HelpSection.tsx
@@ -5,7 +5,7 @@ export const HelpSection = () => {
     <section id="segitseg" className={styles.helpSection}>
       <p className={styles.help}>
         Amennyiben problémád merült fel a vásárlással, az aktiválással vagy a
-        tartalom elérésével kapcsolatban, kérjük írjon egy e-mailt az{' '}
+        tartalom elérésével kapcsolatban, kérjük írj egy e-mailt az{' '}
         <span style={{ textDecoration: 'underline' }}>info@medlino.hu-ra.</span>
       </p>
     </section>
