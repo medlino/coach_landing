2f36ec57d59c40796ddd769d92e2a30bbee58197
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-07-26 12:59:55 +0200

    fix: mobile left/righ space

--- src/components/Sections/RelaxSections/HowItHelpSection/HowItHelpsSection.tsx
+++ src/components/Sections/RelaxSections/HowItHelpSection/HowItHelpsSection.tsx
@@ -13,24 +13,27 @@ export const HowItHelpsSection = () => {
     <section id="hogyan-segit" className={styles.howItHelpsSection}>
       <h1>Hogyan segít egy elvonulás?</h1>
       <p>
+        Gyermekkorunkban nevelés során megtanultuk a létezésünkhöz szükséges
+        alapvető normákat, túlélést szocializálódást segítő elveket de sajnos
+        ugyan úgy azt is hogy egy váratlan stresszhelyeztre alhokollal, vitával,
+        magunkba fordulással, önleértékeléssel reagáljunk. EZEK PROGRAMOK.
+        Folyamatosan figyeltük szüleink és környezetünk visszajelzéseit és
+        eszerint építettük fel az életünket.
+      </p>
+      <p>
         Mi lenne, ha egy olyan környezet venne téged körül, amely felerősíti a
         pozitív tulajdonságaidat, a téged korlátozó berögzült rossz szokásaidra,
         PROGRAMOKRA, MINTÁKRA felhívja a figyelmedet, és segít legyőzni őket.
       </p>
       <p>
-        Mi lenne, ha egy olyan környezet venne téged körül, amely felerősíti a
-        pozitív tulajdonságaidat, a téged korlátozó berögzült rossz szokásaidra
-        felhívja a figyelmedet, és segít legyőzni őket, mert járt már a te
-        cípődben, így hitelesen elfogadod majd tanácsát.
+        Szerinted milyen hamar venne 180 fokot az életed, ha olyan emberektől
+        tanulnál, akik megbízható hiteles tudást és tapasztalatot tudnak
+        nyújtani a problémáidra?
       </p>
       <p>
-        Szerinted milyen hamar venne 180 fokot, ha olyan emberektől tanulnál,
-        akik megbízható hiteles tudást és tapasztalatot tudnak nyújtani a
-        problémáidra?
-      </p>
-      <p>
-        Minden, amire vágysz 1 döntésnyire van. Nincs jó vagy rossz döntés,
-        döntés létezik, az, hogy utána mi történik az csak RAJTAD MÚLIK!
+        Ne feledd, nincs jó vagy rossz döntés azért mert minden döntéssel
+        előrébb jutsz az életben, tehát csak döntés van. Bármit is lépsz hozd ki
+        a legtöbbet a helyzetből mert minden rajtad múlik.
       </p>
       <Button text="CSATLAKOZOM" onClick={() => router.push('/#csomagok')} />
     </section>
